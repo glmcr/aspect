cb7e4fd9614eff6e6a336d8f626092cef8480c9f
glmcr <gmj273@gmail.com>
2025-02-25 18:59:58 -0500

    Now using a vertical velo minimum value to control p.m. ssz asth. creation and modified the condition which controls the creation of new OLM.

--- source/particle/property/lusi_composition.cc
+++ source/particle/property/lusi_composition.cc
@@ -443,9 +443,9 @@ namespace aspect
 	// const bool pm_asth_mrb_type= ( extension_stage && pm_asth_mrb_vvelo_ok) ? true : false;
 	const bool pm_asth_mrb_type= in_extension_stage;
 
-        const bool create_new_olm= (vertical_velo_absv < horiz_velo_absv) ? true : false;
+        const bool create_new_olm= ((vertical_velo < 0.0) || (vertical_velo_absv < horiz_velo_absv)) ? true : false;
 
-        const bool in_decompression= (vertical_velo > 0.0 && (vertical_velo_absv > horiz_velo_absv)) ? true : false;
+        const bool in_decompression= (vertical_velo > ASTH_PARTIAL_MELT_MRB_TYPE_VEL_THRESHOLD); // && (vertical_velo_absv > horiz_velo_absv)) ? true : false;
 
 	// --- Serpentinization parametrization
 	bool metam_fluids_contact_with_olmMRB= false;
