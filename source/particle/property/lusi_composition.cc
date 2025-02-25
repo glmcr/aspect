0c03b19cc19315dc198dc8c9c24055ee5e0e9900
glmcr <gmj273@gmail.com>
2025-02-24 19:03:20 -0500

    quarter of the horiz velo too small.

--- source/particle/property/lusi_composition.cc
+++ source/particle/property/lusi_composition.cc
@@ -451,22 +451,26 @@ namespace aspect
 	// --- Get the horizontal velocity component at the marker position
 	const double horiz_velo= solution[this->Composition<dim>::introspection().component_indices.velocities[0]];
 	
-	// --- Get the vertical velocity component at the marker position
+        const double horiz_velo_absv= std::fabs(horiz_velo);
+
+        // --- Get the vertical velocity component at the marker position
 	const double vertical_velo= solution[this->Composition<dim>::introspection().component_indices.velocities[dim-1]];
 
+        const double vertical_velo_absv= std::fabs(vertical_velo);
+        
 	// --- Determine if the p.m. asth (regardless if it is SZZ or MORB type) needs to be transformed to new OLM material.
 	//     Do not transform the p.m. asth to OLM if the vertical_velo > horiz_velo (in absolute values) but keep the
 	//     p.m. asth of the marker as it is and it could be transformed later to OLM OR to oceanic crust depending on
 	//     its velocity component and also on its location in terms of (p,T) conditions.
         //     NOTE: Works very well in extension mode.
-	const bool create_new_olm= (std::fabs(vertical_velo) < std::fabs(horiz_velo)) ? true : false;
+	const bool create_new_olm= (vertical_velo_absv < horiz_velo_absv) ? true : false;
 
         // --- Determine if the marker is subjected to (significant) decompression by simply checking if it's
-        //     vertical velo is positive and larger than the quarter of its horizontal velo. It is a very
+        //     vertical velo is positive and larger than its horizontal velo. It is a very
         //     ad-hoc and rough parametrization and we would maybe need to use the value of the time derivative
         //     of the pressure or alternatively check if the value of the adiatic heating is negative (which implies
         //     decompression) to have something better ?? 
-        const bool in_decompression= (vertical_velo > 0.0 && (std::fabs(vertical_velo) > 0.25*std::fabs(horiz_velo))) ? true : false;
+        const bool in_decompression= (vertical_velo > 0.0 && (vertical_velo_absv > horiz_velo_absv)) ? true : false;
 	
 	// --- Determine if the vertical velo allows the pm asth. of ssz type.
 	//const bool pm_asth_ssz_vvelo_ok= (vertical_velo > ASTH_PARTIAL_MELT_SSZ_TYPE_VEL_THRESHOLD) ? true: false;
