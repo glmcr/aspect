6fee341126c1d462f090aa971798b1ade163e161
glmcr <gmj273@gmail.com>
2025-09-20 15:08:04 -0400

    Activated the dependance of both the thermal exp. and conductivity (via the thermal diff.) with the Temperature.

--- source/material_model/visco_plastic_lusi.cc
+++ source/material_model/visco_plastic_lusi.cc
@@ -44,7 +44,7 @@ namespace aspect
       // // --- apply the LUSI ad-hoc code that deals with
       // //     the variation of the thermal expansivity and diffusivity
       // //     of the SI materials.
-      //ViscoPlastic<dim>::apply_lusi_thermal_stuff(in,out);
+      ViscoPlastic<dim>::apply_lusi_thermal_stuff(in,out);
       
       // // --- ***IMPORTANT*** Need to add one to the indices
       // //     since the material models properties arrays are always using
